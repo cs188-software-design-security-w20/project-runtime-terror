ad84dc818a4e4766c3ce6d68c37416ac484c3298
Prince <prince112592@yahoo.com>
2020-02-23 17:44:39 -0800

    Webpage Path Changes

--- spotify_server/authorization_code/app.js
+++ spotify_server/authorization_code/app.js
@@ -93,13 +93,13 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/tokenHandler#' +
+        res.redirect('https://princes25.github.io/Mutter/tokenHandler#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }));
       } else {
-        res.redirect('http://localhost:3000/discover#' +
+        res.redirect('https://princes25.github.io/Mutter/discover#' +
           querystring.stringify({
             error: 'invalid_token'
           }));
